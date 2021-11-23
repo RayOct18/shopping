49343840d22f2e4c2a10f4d79c2cc8ca7c7129aa
Ray Xie <birken1018@gmail.com>
2021-11-23 03:43:05 +0000

    feat(docker): build dev db with docker

--- docker/dev/init_db.sh
+++
@@ -1,9 +0,0 @@
-PGPASSWORD='postgres_dev'
-psql -U postgres -f create_db.sql
-psql -U postgres -f setup_db.sql
-
-PGPASSWORD='app_admin'
-psql -U app_admin -d shopping_db -f create_table.sql
-psql -U app_admin -d shopping_db -f create_fk.sql
-psql -U app_admin -d shopping_db -f grant_table_privilege.sq
-psql -U app_admin -d shopping_db -f testing_data.sql
