9075ab807b02ac7dde5af79c5a50e6b0618a249a
Ray <Ray_Xie@wistron.com>
2021-11-29 17:03:32 +0800

    init(src): swagger

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flasgger import Swagger
 
 from config import config
 from utils.logger import set_logger
@@ -10,6 +11,7 @@ def create_app(config_type: str) -> object:
     app = Flask(__name__)
     app.config.from_object(config[config_type])
     set_logger(app)
+    Swagger(app)
 
     @app.route("/")
     def home():
--- src/config.py
+++ src/config.py
@@ -1,5 +1,11 @@
 class BaseConfig:
-    DEBUG = True
+	DEBUG = False
+	SWAGGER = {
+		"title": "Shopping",
+		"version": "0.0.0",
+		"uiversion": 3,
+		"openapi": "3.0.0"
+	}
 
 
 config = {"default": BaseConfig()}
