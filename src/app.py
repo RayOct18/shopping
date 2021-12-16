59c4b28e50140790c887eaab1cad6c50acaf6d44
Ray <Ray_Xie@wistron.com>
2021-12-16 17:01:34 +0800

    chore(src): add some base setting

--- src/app.py
+++ src/app.py
@@ -1,16 +1,15 @@
 import os
+import click
 
-from flask import Flask
+from flask import Flask, g
 from flasgger import Swagger
 
 from config import config
-from utils.logger import set_logger
 
 
-def create_app(config_type: str) -> object:
+def create_app(config: str) -> object:
     app = Flask(__name__)
-    app.config.from_object(config[config_type])
-    set_logger(app)
+    app.config.from_object(config)
     Swagger(app)
 
     @app.route("/")
@@ -18,3 +17,26 @@ def create_app(config_type: str) -> object:
         return "This is Shopping API"
 
     return app
+
+cfg = config['default']
+app = create_app(cfg)
+db = PostgresqlDB(cfg).connect()
+
+
+@app.cli.command()
+@click.option("--module")
+def test(module):
+    import unittest
+    import importlib
+
+    if module:
+        test = importlib.import_module(module)
+        tests = unittest.TestLoader().loadTestsFromModule(test)
+    else:
+        tests = unittest.TestLoader().discover("tests")
+
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- src/config.py
+++ src/config.py
@@ -1,3 +1,21 @@
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+def set_logger(root: str = "logs") -> None:
+    os.makedirs(root, exist_ok=True)
+
+    formatter = logging.Formatter(
+        "[%(asctime)s][%(filename)s:%(lineno)d][%(levelname)s][%(thread)d] - %(message)s"
+    )
+    handler = RotatingFileHandler(
+        os.path.join(root, "app.log"), maxBytes=1024000, backupCount=10
+    )
+    handler.setFormatter(formatter)
+    logging.getLogger().addHandler(handler)
+
+
 class BaseConfig:
     DEBUG = False
     SWAGGER = {
@@ -6,6 +24,11 @@ class BaseConfig:
         "uiversion": 3,
         "openapi": "3.0.0",
     }
+    DB = "shopping_db"
+    DB_USERNAME = os.getenv("db_username", 'app_user')
+    DB_PASSWORD = os.getenv("db_password", 'user_password')
+
+    set_logger()
 
 
 config = {"default": BaseConfig()}
